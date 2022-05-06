b93d482851b059450d74588f482d6b05368415ae
wyszz <88566691+wyszz@users.noreply.github.com>
2022-05-06 17:07:19 +0800

    DT

---
+++ 20220424-3Operations - DT/20220424-3Operations/OfficeClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _20220424_3Operations
+{
+    public class OfficeClass
+    {
+        string _officeid;
+        string _officename;
+        string _schoolid;
+        string _authorizedid;
+        public string OfficeID { get => _officeid; set => _officeid = value; }
+        public string OfficeName { get => _officename; set => _officename = value; }
+        public string SchoolID { get => _schoolid; set => _schoolid = value; }
+        public string Authorizedid { get => _authorizedid; set => _authorizedid = value; }
+    }
+}
