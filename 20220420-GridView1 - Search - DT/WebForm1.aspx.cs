92514cbac016d80d536ab5d74948c9e1a4798b53
wyszz <88566691+wyszz@users.noreply.github.com>
2022-05-06 21:05:48 +0800

    search-DT_ado.net

---
+++ 20220420-GridView1 - Search - DT/WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace _20220420_GridView
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static DataTable DT;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                this.GridView1.DataSource = this.GetTeacherData(null);
+                this.GridView1.DataBind();
+            }
+        }
+        private DataTable GetTeacherData(string condition)
+        {
+            string connectstring = ConfigurationManager.ConnectionStrings["ScoreDBConn"].ConnectionString;
+            SqlConnection sconn = new SqlConnection(connectstring);
+            //string selectstring = "select * from teacher,title,role,authorized,office where teacher.titleid=title.titleid and teacher.roleid=role.roleid and teacher.officeid=office.officeid and teacher.authorizedid=authorized.authorizedid";
+            string selectstring = "select * from view_teacher ";
+            if (condition != null)
+            {
+                selectstring += condition;
+            }
+            SqlDataAdapter sda = new SqlDataAdapter(selectstring, sconn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            DT = dt;
+            return dt;
+        }
+
+        protected void btn_Search_Click(object sender, EventArgs e)
+        {
+            //首先获得tbx的信息，要判断以下是否为空
+            if (this.tbx_Search.Text.Trim() == "")
+            {
+                Response.Write("<script>alert('搜索内容不能为空!')</script>");
+                return;
+            }
+            string tname = this.tbx_Search.Text.Trim();
+            //然后确定单选框，究竟要选的是哪一项
+            string filterexp = null;
+            if (this.rbtnl_SearchMode.SelectedValue == "模糊")
+            {
+                string like = string.Format("%{0}%", tname);
+                filterexp = string.Format("  teachername like '{0}'", like);
+            }
+            else
+            {
+                filterexp = string.Format(" teachername='{0}'", tname);
+            }
+            DataRow[] drs = DT.Select(filterexp);
+            DataTable dt = new DataTable();
+            dt = DT.Clone();
+            foreach (DataRow xdr in drs)
+            {
+                dt.ImportRow(xdr);
+            }
+            this.GridView1.DataSource = dt;
+            this.GridView1.DataBind();
+        }
+    }
+}
